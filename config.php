749339aa45d755a150975c8791eec36514162822
Nirupamvas <54284639+Nirupamvas@users.noreply.github.com>
2020-10-11 15:30:08 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-define('DB_SERVER','localhost');
-define('DB_USER','root');
-define('DB_PASS' ,'');
-define('DB_NAME', 'epiz_26941147_sample');
+define('DB_SERVER','ec2-3-228-114-251.compute-1.amazonaws.comt');
+define('DB_USER','rdokzsyrwuwvqw');
+define('DB_PASS' ,'0786e933890838e35e7ed138b66d356831fb8ef23ffb0652e89ca99628f488f6');
+define('DB_NAME', 'dddlvoacgltgs9');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
 if (mysqli_connect_errno())
