3afd50b5849aa1d57db126b856f6df3d1bc43ee6
Nirupamvas <54284639+Nirupamvas@users.noreply.github.com>
2020-10-11 15:41:01 +0530

    Update config.php

--- config.php
+++ config.php
@@ -3,10 +3,10 @@ define('DB_SERVER','ec2-3-228-114-251.compute-1.amazonaws.comt');
 define('DB_USER','rdokzsyrwuwvqw');
 define('DB_PASS' ,'0786e933890838e35e7ed138b66d356831fb8ef23ffb0652e89ca99628f488f6');
 define('DB_NAME', 'dddlvoacgltgs9');
-$con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
+$con = pg_connect("host=ec2-3-228-114-251.compute-1.amazonaws.com dbname= dddlvoacgltgs9 user =rdokzsyrwuwvqw password= 0786e933890838e35e7ed138b66d356831fb8ef23ffb0652e89ca99628f488f6");
 // Check connection
-if (mysqli_connect_errno())
+if (pg_last_error())
 {
- echo "Failed to connect to MySQL: " . mysqli_connect_error();
+ echo "Failed to connect to MySQL: " . pg_last_error();
 }
 ?>
